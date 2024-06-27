9f7a2f1633153d98d461a8262b9636d9707893f6
dilipdevops5511 <dilipdevopscourses@gmail.com>
2024-06-27 20:35:52 +0530

    oaky

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,4 +1,4 @@
-output "instance_ips" {
-  description = "The public IPs of the created instances."
+output "k6_instance_ips" {
+  description = "The public IPs of the created k6 instances."
   value       = aws_instance.k6_instance.*.public_ip
 }
