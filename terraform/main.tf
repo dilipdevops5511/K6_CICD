69846e980b305e529337457419adab3b30180a46
dilipdevops5511 <dilipdevopscourses@gmail.com>
2024-06-28 09:59:35 +0530

    oaky

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,11 +11,14 @@ resource "aws_subnet" "main" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
   availability_zone = element(var.availability_zones, count.index)
+  
+  map_public_ip_on_launch = true  # Ensure instances launched in this subnet get a public IP
 }
 
 resource "aws_security_group" "instance" {
   vpc_id = aws_vpc.main.id
 
+  // Ingress rules allow SSH and HTTP traffic from anywhere
   ingress {
     from_port   = 22
     to_port     = 22
@@ -30,6 +33,7 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  // Egress rule allows all outbound traffic
   egress {
     from_port   = 0
     to_port     = 0
@@ -44,7 +48,9 @@ resource "aws_instance" "k6_instance" {
   instance_type   = var.instance_type
   subnet_id       = element(aws_subnet.main.*.id, count.index)
   security_groups = [aws_security_group.instance.id]
-  key_name        = "techkeyjune"  
+  key_name        = "techkeyjune"
+  associate_public_ip_address = true  # Ensures instances get a public IP
+
   tags = {
     Name = "k6-instance-${count.index}"
   }
